8f0a9100c908595c9cd8f85ae023e8bdf4818cfc
stevewardle <enquiries@metoffice.gov.uk>
2018-02-22 09:13:52 +0000

    Update version number for new version
    git-svn-id: https://metomi/svn/utils.xm/shumlib/trunk@1740 9980b737-bc2f-46b8-919e-b198a3dff10a

--- common/shumlib_version.h
+++ common/shumlib_version.h
@@ -32,7 +32,7 @@
 // Master definition of version number (uses YYYYMMX format) 
 //                              where "X" is the release number in month MM
 #if !defined(SHUMLIB_VERSION)
-#define SHUMLIB_VERSION 2017101
+#define SHUMLIB_VERSION 2018021
 #endif
 
 // Check the macro we need is defined
