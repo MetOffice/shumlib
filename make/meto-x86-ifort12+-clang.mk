17c51d162b65356d06108ef9c4e61d27a27ba4e3
samclarkegreen <enquiries@metoffice.gov.uk>
2024-06-03 09:16:31 +0000

    #696 : adamvoysey : Whitespace fixes Merged into /shumlib/trunk: /shumlib/branches/dev/adamvoysey/r7324_whitespace_fixes@7328 cf. /shumlib/trunk@7324
    git-svn-id: https://metomi/svn/utils.xm/shumlib/trunk@7464 9980b737-bc2f-46b8-919e-b198a3dff10a

--- make/meto-x86-ifort12+-clang.mk
+++ make/meto-x86-ifort12+-clang.mk
@@ -4,7 +4,7 @@
 # Note that the below are overrides for the main ifort config, due to changes
 # in some of the supported flags
 
-# Also note that it's possible this config will work for versions earlier 
+# Also note that it's possible this config will work for versions earlier
 # than ifort 12 - this was just the earliest version available for testing
 
 # At ifort 15 the option to enable OpenMP was changed to -qopenmp
